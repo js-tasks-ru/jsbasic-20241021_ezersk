402ad500cc9b0b01a5b5a643f7bf760986e9c5e3
Ezerskaya <ezersk@gmail.com>
2024-11-02 21:09:54 +0300

    2-module/task1: sum salary

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,16 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+
+  for (let key in salaries) {
+    let value = Number(salaries[key]);
+
+    if (typeof value === 'number' && value !== Infinity && value !== -Infinity && !isNaN(value)) {
+      sum += value;
+    }
+    /* if (isFinite(value)) {
+      sum += value;
+    } */
+  }
+
+  return sum;
 }
