5b9f946a0e54d3ada3f5fb9d6cf59534b69dd668
Ezerskaya <ezersk@gmail.com>
2024-11-07 18:37:11 +0300

    3-module: without task 3

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,13 @@
 function showSalary(users, age) {
-  // ваш код...
+  let filteredUsers = users
+    .filter((user) => user.age <= age)
+    .map((user, index, newArr) => {
+      if (index !== newArr.length - 1) {
+        return `${user.name}, ${user.balance}\n`;
+      } else {
+        return `${user.name}, ${user.balance}`;
+      }
+    });
+
+  return filteredUsers.join('');
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,11 @@
 function getMinMax(str) {
-  // ваш код...
+  let sortedArray = str
+    .split(' ')
+    .map(el => Number(el))
+    .filter(el => !isNaN(el));
+
+  return {
+    min: Math.min(...sortedArray),
+    max: Math.max(...sortedArray),
+  };
 }
