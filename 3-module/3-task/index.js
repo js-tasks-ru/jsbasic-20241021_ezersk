87065b558caf7a4a716c713d7dec748d44dcc5a3
Ezerskaya <ezersk@gmail.com>
2024-11-09 16:34:13 +0300

    3-module: 3-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,13 @@
 function camelize(str) {
-  // ваш код...
+  let transformWord = str
+    .split('-')
+    .map((word, index) => {
+      if (index === 0) { return word; }
+
+      return word.charAt(0).toUpperCase() + word.slice(1);
+
+    })
+    .join('');
+
+  return transformWord;
 }
